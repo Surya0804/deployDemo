57bc9a6964ecfa9a55d0a8c0c9fbff89721f8423
Surya Teja Vuppuluri <suryateja.vuppuluri@valuemomentum.com>
2024-05-30 12:10:05 +0530

    success

--- src/main/java/com/example/practice/deploydemo/controller/EmployeeController.java
+++ src/main/java/com/example/practice/deploydemo/controller/EmployeeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "https://beamish-unicorn-03269b.netlify.app/")
 public class EmployeeController {
 
     @Autowired
